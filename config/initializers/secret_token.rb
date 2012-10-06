a1b5c354b06d9ac88c2c201d400c19f82ab4b611
Nalin <nalin@Nalins-MacBook-Pro.local>
2012-10-06 14:36:36 -0700

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookapp::Application.config.secret_token = 'e9d4fac1d62452571854c2d43c98a1fd29bc217604623fa3e6453457d466482ef2b0675567c41a73189269beab933ae5fcd3d87f3705c6a2666e6a0cf7d2df5d'
